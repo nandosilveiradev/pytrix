83fb64f4448a3db7c334ed32aa793407d2d7eb88
Fernando Silveira <fernandosilveiradev@gmail.com>
2026-04-16 05:33:12 -0300

    build: implement pyproject.toml and transition to editable package layout

--- pytrix.py
+++ pytrix.py
@@ -1,12 +1,9 @@
 """
-app.py(legacy) : 
+app.py (legacy) : 
 pytrix.py 
 Enquanto instancia nativa pytrix.py, ao rodar forge o nome do pytrix.py será modificado para nome do utilitário
-fica a criterio do usuario manter o nome apos forge
-
-
+fica a critério do usuário manter o nome após forge
 """
-
 
 from pytrix_controllers.pytrix_controllers_select_language import PytrixControllerSelectLanguage
 
@@ -25,22 +22,29 @@ class App:
         #     from utils.tmux import start_or_attach_session
         #     start_or_attach_session()
         #     sys.exit(0)  # encerra aqui, porque o attach já cuida de rodar
+
         # roda seleção de idioma e imprime o idioma escolhido
         print("Agora vem a escolha do idioma: log ")
-        idioma : str = self.select_language.run()
+        idioma: str = self.select_language.run()
         print(f"{idioma}")
+
         # exemplo de como acessar mensagens traduzidas do dicionário
-        #print(I18N["arrow_instructions"][self.select_language.lang])
+        # print(I18N["arrow_instructions"][self.select_language.lang])
 
         # aqui entraria ControllersModeServer se ativado
         # self.mode_server: ControllersModeServer = ControllersModeServer(self.select_language.lang)
         # self.mode_server.run()
 
-        return
+        return idioma
+
+
+def main():
+    # função usada como entrypoint global (via pyproject.toml)
+    app = App()
+    app.run()
 
 
 if __name__ == "__main__":
- 
     # instancia a aplicação principal
     app = App()
     app.run()
